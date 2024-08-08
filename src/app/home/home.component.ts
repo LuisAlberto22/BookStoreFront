abf3a74bc0aaed9b393fa452d07b37dc8222fc89
Luis Alberto Garcia Orozco <guichos-mx@outlook.com>
2024-08-08 14:50:28 -0600

    Creacion de la pagina principal

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { CardComponent } from '../shared/components/card/card.component';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeaderComponent, CardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
