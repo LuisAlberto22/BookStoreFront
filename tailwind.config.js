bc1006a67fbf9db3e1acae2e6edd009974bb710e
Luis Alberto Garcia Orozco <guichos-mx@outlook.com>
2024-08-08 14:49:55 -0600

    Agregacion de custom colores y boxshadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+
+    extend: {
+      boxShadow: {
+        'custom': '0 0 20px rgba(0, 0, 0, 0.5)',
+      },
+      keyframes: {
+        pomp: {
+          '0%': { transform: 'scale(1)' },
+          '100%': { transform: 'scale(1.2)' },
+        },
+      },
+      animation:{
+        pomp: 'pomp 0.3s ease-in-out',
+      },
+      colors:{
+        'main-color' : '#111827',
+        'text-color' : '#ffffff',
+      },
+    },
   },
   plugins: [],
 }
